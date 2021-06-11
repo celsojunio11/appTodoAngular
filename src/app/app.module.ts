68ff4fb8ad05cc847bfb0e8791fc35f318edb4ef
celsojunio11 <celsojunio11@gmail.com>
2021-06-10 21:38:50 -0300

    Subindo projeto

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormValidateComponent } from './form-validate/form-validate.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormValidateComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/form-validate/form-validate.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-validate',
+  templateUrl: './form-validate.component.html',
+  styleUrls: ['./form-validate.component.css']
+})
+export class FormValidateComponent implements OnInit {
+
+nome: string = "";
+email: string = "";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
